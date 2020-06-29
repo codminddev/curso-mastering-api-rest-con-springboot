8c7dbb3a484c379b58882d0c8a21c6ddc7795213
oblancarte <oscar_jb1@hotmail.com>
2020-06-28 21:00:04 -0500

    Crear nuestro primer API REST (Hello World)

---
+++ src/main/java/com/codmind/orderapi/controllers/HelloWorldController.java
@@ -0,0 +1,17 @@
+package com.codmind.orderapi.controllers;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class HelloWorldController {
+	
+	
+	@GetMapping(value="hello")
+	public String hello() {
+		return "Hello world";
+	}
+	
+}
