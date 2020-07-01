643b4d3f700852f9238230798afc18c9c10ab466
oblancarte <oscar_jb1@hotmail.com>
2020-06-30 20:06:23 -0500

    creando la entidad Product

--- src/main/java/com/codmind/orderapi/entity/Product.java
+++ src/main/java/com/codmind/orderapi/entity/Product.java
@@ -1,13 +1,30 @@
 package com.codmind.orderapi.entity;
 
+import javax.persistence.*;
+
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@NoArgsConstructor
+@AllArgsConstructor
 @Getter
 @Setter
 @Builder
+@Entity
+@Table(name="PRODUCTS")
 public class Product {
+	
+	@Id
+	@Column(name="ID")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
+	@Column(name="NAME", nullable = false, length = 100)
 	private String name;
+	
+	@Column(name="PRICE", nullable = false)
+	private Double price;
 }
