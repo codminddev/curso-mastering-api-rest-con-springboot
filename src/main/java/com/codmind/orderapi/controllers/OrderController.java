0cb68ed5d1e3c78a1dbb989b30afa6b50b0f6c7d
oblancarte <oscar_jb1@hotmail.com>
2020-07-04 15:44:57 -0500

    Creando el Controller de Ordenes

---
+++ src/main/java/com/codmind/orderapi/controllers/OrderController.java
@@ -0,0 +1,69 @@
+package com.codmind.orderapi.controllers;
+
+import java.util.List;
+
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.codmind.orderapi.converters.OrderConverter;
+import com.codmind.orderapi.dtos.OrderDTO;
+import com.codmind.orderapi.entity.Order;
+import com.codmind.orderapi.utils.WrapperResponse;
+
+@RestController
+public class OrderController {
+	
+	private OrderConverter converter = new OrderConverter();
+
+	@GetMapping(value="/orders")
+	public ResponseEntity<WrapperResponse<List<OrderDTO>>> findAll(
+			@RequestParam(name = "pageNumber", required = false, defaultValue = "0") int pageNumber,
+			@RequestParam(name = "pageSize", required = false, defaultValue = "5") int pageSize
+			){
+		
+		Pageable page = PageRequest.of(pageNumber, pageSize);
+		
+		List<Order> orders = null; //orderService.findAll(page);
+		
+		
+		return new WrapperResponse<>(true, "success", converter.fromEntity(orders))
+				.createResponse();
+	}
+	
+	@GetMapping(value = "/orders/{id}")
+	public ResponseEntity<WrapperResponse<OrderDTO>> findById(@PathVariable(name="id") Long id){
+		Order order = null; // orderService.findById(id);
+		return new WrapperResponse<>(true, "success", converter.fromEntity(order))
+				.createResponse();
+	}
+	
+	@PostMapping(value="/orders")
+	public ResponseEntity<WrapperResponse<OrderDTO>> create(@RequestBody OrderDTO order){
+		Order newOrder = null; // orderService.save(converter.fromDTO(order));
+		return new WrapperResponse<>(true, "success", converter.fromEntity(newOrder))
+				.createResponse();
+	}
+	
+	@PutMapping(value="/orders")
+	public ResponseEntity<WrapperResponse<OrderDTO>> update(@RequestBody OrderDTO order){
+		Order newOrder = null; // orderService.save(converter.fromDTO(order));
+		return new WrapperResponse<>(true, "success", converter.fromEntity(newOrder))
+				.createResponse();
+	}
+	
+	@DeleteMapping(value="/orders/{id}")
+	public ResponseEntity<?> delete(@PathVariable(name="id") Long id){
+		//orderService.delete(id);
+		return new WrapperResponse<>(true, "success", null).createResponse();
+	}
+}
--- src/main/java/com/codmind/orderapi/utils/WrapperResponse.java
+++ src/main/java/com/codmind/orderapi/utils/WrapperResponse.java
@@ -17,6 +17,11 @@ public class WrapperResponse<T> {
 	private String message;
 	private T body;
 	
+	
+	public ResponseEntity<WrapperResponse<T>> createResponse(){
+		return new ResponseEntity(HttpStatus.OK);
+	}
+	
 	public ResponseEntity<WrapperResponse<T>> createResponse(HttpStatus status){
 		return new ResponseEntity(this,status);
 	}
