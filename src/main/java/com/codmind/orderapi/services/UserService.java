ceb16c0a8def504b0f17d117d2d1359481a3d184
oblancarte <oscar_jb1@hotmail.com>
2020-07-07 22:58:29 -0500

    Crear el token JWT

--- src/main/java/com/codmind/orderapi/services/UserService.java
+++ src/main/java/com/codmind/orderapi/services/UserService.java
@@ -1,6 +1,9 @@
 package com.codmind.orderapi.services;
 
+import java.util.Date;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import com.codmind.orderapi.converters.UserConverter;
@@ -13,11 +16,20 @@ import com.codmind.orderapi.exceptions.ValidateServiceException;
 import com.codmind.orderapi.repository.UserRespository;
 import com.codmind.orderapi.validators.UserValidator;
 
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.SignatureException;
+import io.jsonwebtoken.UnsupportedJwtException;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Service
 public class UserService {
+	
+	@Value("${jwt.password}")
+	private String jwtSecret;
 	
 	@Autowired
 	private UserConverter userConverter;
@@ -52,7 +64,10 @@ public class UserService {
 					.orElseThrow(() -> new ValidateServiceException("Usuario o password incorrectos"));
 			
 			if(! user.getPassword().equals(request.getPassword())) throw new ValidateServiceException("Usuario o password incorrectos");
-			return new LoginResponseDTO(userConverter.fromEntity(user), "TOKEN");
+			
+			String token = createToken(user);
+			
+			return new LoginResponseDTO(userConverter.fromEntity(user), token);
 		} catch (ValidateServiceException | NoDataFoundException e) {
 			log.info(e.getMessage(), e);
 			throw e;
@@ -60,6 +75,34 @@ public class UserService {
 			log.error(e.getMessage(), e);
 			throw new GeneralServiceException(e.getMessage(), e);
 		}
+	}
+	
+	public String createToken(User user) {
+		Date now = new Date();
+		Date expiryDate = new Date(now.getTime() + (1000*60*60));
+		
+		return Jwts.builder()
+				.setSubject(user.getUsername())
+				.setIssuedAt(now)
+				.setExpiration(expiryDate)
+				.signWith(SignatureAlgorithm.HS512, jwtSecret)
+				.compact();
+	}
+	
+	public boolean validateToken(String token) {
+		try {
+			Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token);
+			return true;
+		}catch (UnsupportedJwtException e) {
+			log.error("JWT in a particular format/configuration that does not match the format expected");
+		}catch (MalformedJwtException e) {
+			log.error(" JWT was not correctly constructed and should be rejected");
+		}catch (SignatureException e) {
+			log.error("Signature or verifying an existing signature of a JWT failed");
+		}catch (ExpiredJwtException e) {
+			log.error("JWT was accepted after it expired and must be rejected");
+		}
+		return false;
 	}
 	
 }
